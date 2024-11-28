ff1d0c807aa7a9069089c400c03febcd1e8fb47b
C0SM0ik <sviatoslav.mazur13@gmail.com>
2024-11-28 17:02:35 +0200

    Add files via upload

---
+++ test/memo_qss.py
@@ -0,0 +1,42 @@
+style_sheet = """   
+                  QApplication {
+                      background: #393A5A;
+                      color: #ffffff;
+                  }
+                  QWidget {
+                      background: #3D3E40;
+                      color: #ffffff;
+                  }
+                  QPushButton {
+                      background: #393A5A;
+                      border: 3px solid black;
+                      border-radius: 7px;
+                      font-family: Comic Sans, sans-serif;
+                      font-size: 20px;
+                      font-weight: bold;
+                  }
+                  QGroupBox {
+                      background: #525759;
+                      border: 3px solid black;
+                      border-radius: 7px;
+                      color: #525759;
+                  }
+                  QRadioButton {
+                      background: #393A5A;
+                      border: 3px solid black;
+                      border-radius: 7px;
+                      font-family: Comic Sans, sans-serif;
+                      font-size: 20px;
+                      font-weight: bold;
+                      padding: 10px 0 10px 10px;
+                  }
+                  QLabel {
+                      background: #393A5A;
+                      color: white;
+                      font-family: Comic Sans, sans-serif;
+                      font-size: 25px;
+                      border: 3px solid black;
+                      border-radius: 7px;
+                      
+                  }
+                  """
